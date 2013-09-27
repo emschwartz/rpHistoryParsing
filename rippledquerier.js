544c855980cd968723d31d4c26fda014ccb7c8f6
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-09-27 15:35:02 -0700

    testing

--- rippledquerier.js
+++ rippledquerier.js
@@ -140,6 +140,8 @@ function dbRecursiveSearch(db, table, index, start, end, key, val, callback) {
                 return;
             }
 
+            winston.info("rows.length", rows.length);
+
             for (var i = 0; i < rows.length - 1; i++) {
                 if (rows[i][key] <= val && val < rows[i + 1][key]) {
                     winston.info("Found value between index", rows[i][index], "and", rows[i + 1][index]);
