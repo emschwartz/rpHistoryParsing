4b3290241fe78a1999ecb36d0c89876cfef76194
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-09-27 13:52:50 -0700

    testing

---
+++ test.js
@@ -0,0 +1,32 @@
+var winston = require('winston'),
+    ripple = require('ripple-lib'),
+    moment = require('moment'),
+    _ = require('lodash'),
+    async = require('async'),
+    knox = require('knox'),
+    RippledQuerier = require('./rippledquerier').RippledQuerier;
+
+var config = require('./config');
+
+var client = knox.createClient({
+    key: config.s3.key,
+    secret: config.s3.secret,
+    bucket: config.s3.bucket
+});
+
+var MAX_UPLOADERS = 25;
+
+
+
+var rq = new RippledQuerier(1000);
+
+
+rq.getLedgersByTimeRange(moment(), moment().subtract("hours", 1), function(err, ledgers){
+    if (err) {
+        winston.error(err);
+        return;
+    }
+
+    winston.info("Got this many ledgers:", ledgers.length);
+
+});
