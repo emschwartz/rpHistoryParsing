6dc113078ee4e2fe09518530915ea78c5879866f
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-09-26 19:15:27 -0700

    added ripple-lib

--- rippledquerier.js
+++ rippledquerier.js
@@ -15,64 +15,62 @@ var testrq = new RippledQuerier("/ripple/server/db");
 testrq.getLedger(20000000);
 
 
-var RippledQuerier = (function(db_url){
+var RippledQuerier = (function(db_url) {
 
-	var rq = {};
+    var rq = {};
 
-	var txdb = new sqlite3.Database(path.resolve(config.dbPath || ".", 'transaction.db'));
+    var txdb = new sqlite3.Database(path.resolve(config.dbPath || ".", 'transaction.db'));
     var ledb = new sqlite3.Database(path.resolve(config.dbPath || ".", 'ledger.db'));
 
-    function printCallback(err, result){
-    	if (err) {
-    		winston.error(err);
-    	} else {
-    		winston.info(result);
-    	}
+    function printCallback(err, result) {
+        if (err) {
+            winston.error(err);
+        } else {
+            winston.info(result);
+        }
     }
 
-    function getRawLedger(ledger_index, callback){
-    	if (!callback) callback = printCallback;
+    function getRawLedger(ledger_index, callback) {
+        if (!callback) callback = printCallback;
 
-    	ledb.all("SELECT * FROM Ledgers WHERE LedgerSeq = ?;", 
-    			[ledger_index],
-    			function(err, rows){
-    				if (err) {
-    					callback(err);
-    					return;
-    				}
+        ledb.all("SELECT * FROM Ledgers WHERE LedgerSeq = ?;", [ledger_index],
+            function(err, rows) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
 
-    				if (ledb_rows.length === 0) {
-			            callback(new Error("ledb has no ledger of index: " + ledger_index));
-			            return;
-			        }
+                if (ledb_rows.length === 0) {
+                    callback(new Error("ledb has no ledger of index: " + ledger_index));
+                    return;
+                }
 
-    				if (rows.length > 1) {
-    					winston.error("ledb has more than 1 entry for ledger_index:", ledger_index, "continuing anyway");
-    				}
+                if (rows.length > 1) {
+                    winston.error("ledb has more than 1 entry for ledger_index:", ledger_index, "continuing anyway");
+                }
 
-    				callback(null, rows[0]);
-    			});
+                callback(null, rows[0]);
+            });
     }
 
-    function getRawTxForLedger(ledger_index, callback){
-    	if (!callback) callback = printCallback;
+    function getRawTxForLedger(ledger_index, callback) {
+        if (!callback) callback = printCallback;
 
-    	txdb.all("SELECT * FROM Ledgers WHERE LedgerSeq = ?;",
-    			[ledger_index],
-    			function(err, rows){
-    				if (err) {
-    					callback(err);
-    					return;
-    				}
+        txdb.all("SELECT * FROM Ledgers WHERE LedgerSeq = ?;", [ledger_index],
+            function(err, rows) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
 
-    				callback(null, rows);
-    			});
+                callback(null, rows);
+            });
     }
 
-    rq.getLedger = function(ledger_index, callback){
+    rq.getLedger = function(ledger_index, callback) {
 
-    	getRawLedger(callback);
-    	getRawTxForLedger(callback);
+        getRawLedger(callback);
+        getRawTxForLedger(callback);
 
     };
 
