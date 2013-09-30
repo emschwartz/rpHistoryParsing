da38a5dff4690cfdf17d473e1d737e86e24fdce9
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-09-30 10:11:03 -0700

    testing parsing

--- test.js
+++ test.js
@@ -43,3 +43,79 @@ rq.getLedgerRange(1297768, 1297769);
 
 // rq.searchLedgerByClosingTime(433630980);
 
+downloadLedger(1297768, function(ledger){
+    winston.info(ledger);
+    winston.info(ledger.transactions.length);
+});
+
+function downloadLedger (ledger_num, callback) {
+
+        client.get('ledgers/' + ledger_num + '.json').on('response', function(res) {
+
+            var dataBuffer = '';
+
+            if (404 === res.statusCode) {
+
+                // winston.error('ledger/' + ledger_num + '.json doesn\'t exist, statusCode:', res.statusCode);
+                // callback();
+                // return;
+
+            }
+
+            if (500 === res.statusCode) {
+
+                winston.error("got statusCode 500 for ledger:", ledger_num, ", retrying in 1 sec");
+                setTimeout(function(){
+                    downloadLedger(ledger_num, callback);
+                }, 1000);
+
+            }
+
+            if (200 === res.statusCode) {
+
+                res.on('error', function(){
+
+                    winston.error("error while downloading ledger: " + ledger_num + " retrying in 1 sec");
+                    setTimeout(function(){
+                        downloadLedger(ledger_num, callback);
+                    }, 1000);
+
+                });
+
+                res.setEncoding('utf8');
+
+                res.on('data', function(data) {
+                    dataBuffer += data;
+                });
+
+                res.on('end', function() {
+
+                    var ledger;
+
+                    try {
+
+                        ledger = JSON.parse(dataBuffer);
+
+
+                    } catch (e) {
+
+                        winston.error("Malformed JSON at: ledger/" + ledger_num + ".json, trying again in 1 sec");
+                        setTimeout(function(){
+                            downloadLedger(ledger_num, callback);
+                        }, 1000);
+
+                    }
+
+                    if (ledger.ledger_index !== ledger_num) {
+                        callback(new Error("Ledger index for file " + ledger_num + ".json is " + ledger.ledger_index));
+                        return;
+                    }
+
+                    callback(null, ledger);
+
+                });
+
+            }
+        }).end(); // finish the client.get request
+
+}
