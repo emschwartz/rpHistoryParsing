615b4e0a4fa2bd4ef222a4c0ed62ada7350f139a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-09-30 17:32:04 -0700

    editing mapreducer

--- mapreducer.js
+++ mapreducer.js
@@ -26,11 +26,9 @@ function applyToRange (first_index, last_index, iterator, callback) {
         return;
     }
 
-    winston.info("applying to ledgers from", first_index, "to", last_index);
-
     var indices = _.range(first_index, last_index);
 
-    async.each(indices, function(index, async_callback){
+    async.eachLimit(indices, MAX_ITERATORS, function(index, async_callback){
 
         rq.getLedger(index, function(err, ledger){
             if (err) {
@@ -50,17 +48,82 @@ function applyToAll (iterator, callback) {
 }
 
 
+function mapOverRange (first_index, last_index, iterator, callback) {
 
-applyToRange(2400000, "LAST", function(ledger, async_callback){
+    if (first_index === null || first_index === "FIRST" || first_index === "first") {
+        first_index = rq.FIRST_INDEX;
+    }
 
-    winston.info("Ledger", ledger.ledger_index, "has", ledger.transactions.length, "transactions");
-    async_callback();
+    if (last_index === null || last_index === "LAST" || last_index === "last") {
+        rq.getLatestLedgerIndex(function(err, rq_last_index){
+            if (err) {
+                callback(err);
+                return;
+            }
+            mapOverRange(first_index, rq_last_index, iterator, callback);
+        });
+        return;
+    }
+
+    var indices = _.range(first_index, last_index);
+
+
+    async.mapLimit(indices, MAX_ITERATORS, function(index, async_callback){
+
+        rq.getLedger(index, function(err, ledger){
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            iterator(ledger, async_callback);
+        });
+
+    }, callback);
+
+}
+
+function mapOverAll (iterator, callback) {
+    mapOverRange("FIRST", "LAST", iterator, callback);
+}
+
+applyToAll(function(ledger, async_callback){
+
+    var row = ledger.ledger_index + ", " + ledger.close_time_human + ", " + ledger.transactions.length) + "\n";
+
+    fs.appenFile("tx_history.csv", row, async_callback);
+
 
 }, function(err){
 
-    if (err) {
+    if (err){
         winston.error(err);
         return;
     }
 
 });
+
+// mapOverAll(function(ledger, async_callback){
+
+// }, function(err, results){
+
+//     for (var r = 0, len = results.length; r < len; r++) {
+//         fs.appenFile("tx_history.csv", (results[r].ledger_index + ", " + results[r].close_time_human + results[r].transactions.length))
+//     }
+
+// });
+
+
+// applyToRange(2400000, "LAST", function(ledger, async_callback){
+
+//     winston.info("Ledger", ledger.ledger_index, "has", ledger.transactions.length, "transactions");
+//     async_callback();
+
+// }, function(err){
+
+//     if (err) {
+//         winston.error(err);
+//         return;
+//     }
+
+// });
