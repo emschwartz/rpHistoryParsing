48cf4c76e7aaa97d4d3a1e02954851b7752a4b27
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-04 14:10:38 -0700

    testing batch saving to couchdb

--- rippledtocouchdb.js
+++ rippledtocouchdb.js
@@ -1,12 +1,17 @@
 var couchdb = require('felix-couchdb'),
     client = couchdb.createClient(5984, 'localhost'),
     db = client.db('rphistory'),
-    queue = require('queue-async');
+    queue = require('queue-async'),
+    async = require('async'),
+    _ = require('lodash');
 
 var RippledQuerier = require('./rippledquerier'),
     rq = new RippledQuerier();
 
 var winston = require('winston');
+
+var MAX_ITERATORS = 1000;
+var BATCH_SIZE = 1000;
 
 
 // db.getDoc("cc0a5edcc25f1a1ec960c1dc8a2a2030", function(err, doc){
@@ -18,21 +23,59 @@ var winston = require('winston');
 //     winston.info(doc);
 // });
 
-var ledger_index = 2000000;
+saveBatch(2000000);
 
-rq.getLedger(ledger_index, function(err, ledger){
-    if (err) {
-        winston.error(err);
-        return;
-    }
 
-    db.saveDoc(ledger_index, ledger, function(err, ok){
-        if (err) {
-            winston.error(err);
-            return;
-        }
-        winston.info("Ledger", ledger_index, "saved to CouchDB");
+function saveBatch (batch_start, callback) {
+    rq.getLatestLedgerIndex(function(err, latest_ledger_index){
+
+        var batch_end = Math.min(latest_ledger_index, batch_start + BATCH_SIZE);
+        winston.info("Saving batch from", batch_start, "to", batch_end);
+        var incides = _.range(batch_start, batch_end);
+
+        rq.getLedgerRange(batch_start, batch_end, function(err, ledgers){
+            if (err) {
+                winston.error("Error saving batch from", batch_start, "to", batch_end, ":", err);
+                return;
+            }
+
+            async.eachLimit(ledgers, MAX_ITERATORS, function(ledger, async_callback){
+
+                db.saveDoc(ledger.ledger_index, ledger, function(err, ok){
+                    if (err) {
+                        async_callback(err);
+                        return;
+                    }
+                    async_callback(null);
+                });
+
+            }, function(err){
+                if (err) {
+                    winston.error("Error saving batch from", batch_start, "to", batch_end, ":", err);
+                    return;
+                }
+
+                callback(null, batch_end);
+            });
+
+        });
+
     });
+}
 
-});
+// rq.getLedger(ledger_index, function(err, ledger){
+//     if (err) {
+//         winston.error(err);
+//         return;
+//     }
+
+//     db.saveDoc(ledger_index, ledger, function(err, ok){
+//         if (err) {
+//             winston.error(err);
+//             return;
+//         }
+//         winston.info("Ledger", ledger_index, "saved to CouchDB");
+//     });
+
+// });
 
