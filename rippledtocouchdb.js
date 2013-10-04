a7648018512ffeef870fed0a9fa33d10687c266c
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-04 13:51:52 -0700

    saving test ledger to couchdb

--- rippledtocouchdb.js
+++ rippledtocouchdb.js
@@ -3,14 +3,36 @@ var couchdb = require('felix-couchdb'),
     db = client.db('rphistory'),
     queue = require('queue-async');
 
+var RippledQuerier = require('./rippledquerier'),
+    rq = new RippledQuerier();
+
 var winston = require('winston');
 
 
-db.getDoc("cc0a5edcc25f1a1ec960c1dc8a2a2030", function(err, doc){
+// db.getDoc("cc0a5edcc25f1a1ec960c1dc8a2a2030", function(err, doc){
+//     if (err) {
+//         winston.error(err);
+//         return;
+//     }
+
+//     winston.info(doc);
+// });
+
+var ledger_index = 2000000;
+
+rq.getLedger(ledger_index, function(err, ledger){
     if (err) {
         winston.error(err);
         return;
     }
 
-    winston.info(doc);
+    db.saveDoc(ledger_index, ledger, function(err, ok){
+        if (err) {
+            winston.error(err);
+            return;
+        }
+        winston.info("Ledger", ledger_index, "saved to CouchDB");
+    });
+
 });
+
