7dbcb8841ebd4ae359cc42da3a1e1c2b2600a4eb
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-04 12:46:49 -0700

    edited test

--- rippledtocouchdb.js
+++ rippledtocouchdb.js
@@ -4,7 +4,4 @@ var couchdb = require('felix-couchdb'),
     queue = require('queue-async');
 
 
-var req = db.getDoc("aecd76d84ad9af8fdcb6c39cfb000bde");
-req.on('response', function(res){
-    winston.info(res);
-});
+winston.info(db.getDoc("aecd76d84ad9af8fdcb6c39cfb000bde"));
