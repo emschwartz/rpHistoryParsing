676917c3d83f3b0393ec9585a47d8ac6f655a142
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-17 16:22:55 -0700

    fixed function to get last ledger saved from couchdb to account for most recent changes having been design docs

--- rippledtocouchdb.js
+++ rippledtocouchdb.js
@@ -33,6 +33,9 @@ db.changes({
 
     winston.info(JSON.stringify(res));
 
+    winston.info(JSON.parse(res.results[0].id));
+    winston.info(JSON.parse(res.results[3].id));
+
     // find last saved ledger amongst couchdb changes stream
     var last_saved_index;
     for (var r = 0; r < res.results.length; r++) {
