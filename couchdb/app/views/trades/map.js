871aae8e82726608ae81c16790fb25e4dc7ba6cf
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-22 17:42:21 -0700

    working on couchdb views

---
+++ couchdb/app/views/trades/map.js
@@ -0,0 +1,8 @@
+function (doc) {
+    for (var t = 0, txs = doc.transactions.length; t < txs; t++) {
+        var tx = doc.transactions[t];
+
+        
+
+    }
+}
--- couchdb/app/views/trustlinesByAccount/reduce.js
+++ couchdb/app/views/trustlinesByAccount/reduce.js
@@ -15,27 +15,28 @@ function (keys, values, rereduce) {
         
     }
 
-    function compactRows (accounts, trustlines) {
+    function compactRows (accounts, partial_lines) {
         var acct_lines = {};
 
-        for (var k = 0, len = accounts.length; k < len; k++) {
+        for (var k = 0, num_accts = accounts.length; k < num_accts; k++) {
             var acct = accounts[k],
-                currencies = Object.accounts(trustlines[k]);
+                partial_line = partial_lines[k],
+                currencies = Object.keys(partial_line);
 
             if (typeof acct_lines[acct] === "undefined")
                 acct_lines[acct] = {};
 
             for (var c = 0; c < currencies.length; c++) {
                 var curr = currencies[c];
-                if (acct_lines[acct][curr] === "undefined")
+                if (typeof acct_lines[acct][curr] === "undefined")
                     acct_lines[acct][curr] = {"inc_lines": 0, "out_lines": 0, "balance_change": 0};
 
-                if (typeof trustlines[k][curr].inc_lines === "number")
-                    acct_lines[acct][curr].inc_lines += trustlines[k][curr].inc_lines;
-                if (typeof trustlines[k][curr].out_lines === "number")
-                    acct_lines[acct][curr].out_lines += trustlines[k][curr].out_lines;
-                if (typeof trustlines[k][curr].balance_change === "number")
-                    acct_lines[acct][curr].balance_change += trustlines[k][curr].balance_change;
+                if (typeof partial_line[curr].inc_lines === "number")
+                    acct_lines[acct][curr].inc_lines += partial_line[curr].inc_lines;
+                if (typeof partial_line[curr].out_lines === "number")
+                    acct_lines[acct][curr].out_lines += partial_line[curr].out_lines;
+                if (typeof partial_line[curr].balance_change === "number")
+                    acct_lines[acct][curr].balance_change += partial_line[curr].balance_change;
             }
         }
 
