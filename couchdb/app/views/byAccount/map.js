f147dd808e077f2eb4a517386473ab5baf86d33a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-10-24 18:20:58 -0700

    cleaning deleted files

--- couchdb/app/views/byAccount/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-  var transactions = doc.transactions,
-      timestamp = doc.close_time_timestamp;
-  for (var i = 0, n = transactions.length; i < n; ++i) {
-    var t = transactions[i];
-    if (t.TransactionType !== "Payment") continue;
-    emit([t.Account, t.Destination], i);
-  }
-}
