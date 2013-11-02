801872644b6eb04058afa6a38a197da106341fd8
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-11-02 12:30:07 -0700

    fixed a bug with the offersExercised view

--- couchdb/app/views/offersExercised/reduce.js
+++ couchdb/app/views/offersExercised/reduce.js
@@ -3,25 +3,40 @@ function (keys, values, rereduce) {
     if (!rereduce) {
 
         var stats = {};
-        stats.open = (values[0].length === 3 ? values[0][2] : values[0][0] / values[0][1]);
-        stats.close = (values[values.length-1].length === 3 ? values[values.length-1][2] : values[values.length-1][0] / values[values.length-1][1]);
+        var first_price = (values[0].length === 3 ? values[0][2] : values[0][0] / values[0][1]);
 
-        // starting values for other stats
+        stats.open_time = keys[0].splice(2);
+        stats.open = first_price;
+        stats.close_time = keys[0].splice(2);
+        stats.close = first_price;
+
         stats.high = stats.open;
         stats.low = stats.open;
+
         stats.curr1_vwav_numerator = 0;
         stats.curr1_volume = 0;
         stats.curr2_volume = 0;
+
         stats.num_trades = 0;
 
-        values.forEach(function(trade){
+        values.forEach(function(trade, index){
             var rate = (trade.length === 3 ? trade[2] : trade[0] / trade[1]);
+            
+            if (keys[index].splice(2) < stats.open_time) {
+                stats.open_time = keys[index].splice(2);
+                stats.open = rate;
+            }
+            if (keys[index].splice(2) > stats.close_time) {
+                stats.close_time = keys[index].splice(2);
+                stats.close = rate;
+            }
+
             stats.high = Math.max(stats.high, rate);
             stats.low = Math.min(stats.low, rate);
             stats.curr1_vwav_numerator += rate * trade[0];
             stats.curr1_volume += trade[0];
             stats.curr2_volume += trade[1];
-            stats.num_trades ++; 
+            stats.num_trades++;
         });
 
         stats.volume_weighted_avg = stats.curr1_vwav_numerator / stats.curr1_volume;
@@ -30,10 +45,27 @@ function (keys, values, rereduce) {
 
     } else {
 
-        var stats = values[0];
-        stats.close = values[values.length - 1].close;
+        var stats = {
+            open_time: values[0].open_time,
+            open: values[0].open,
+            close_time: values[0].close_time,
+            close: values[0].close,
+            curr1_vwav_numerator: 0,
+            curr1_volume: 0,
+            curr2_volume: 0,
+            num_trades: 0
+        };
 
         values.forEach(function(segment){
+            if (segment.open_time < stats.open_time) {
+                stats.open_time = segment.open_time;
+                stats.open = segment.open;
+            }
+            if (segment.close_time > stats.close_time) {
+                stats.close_time = segment.close_time;
+                stats.close = segment.close;
+            }
+
             stats.high = Math.max(stats.high, segment.high);
             stats.low = Math.min(stats.low, segment.low);
 
